65eeda1f5b73accf4654abbad2ed15b9977c068a
XtrEEmWasTaken <99416809+XtrEEmWasTaken@users.noreply.github.com>
2024-10-24 12:59:32 -0400

    Text Prompt

--- Assets/Scripts/Doors/Door 1.cs
+++ Assets/Scripts/Doors/Door 1.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using TMPro;
 
 public class Door1 : MonoBehaviour
 {
+    public TextMeshProUGUI promptText;
+    public string FirstDoor = "Passed to the next level";
+    public string SecondDoor = "You Died";
+    public string ThirdDoor = "Retreat";
+
+    void Start()
+    {
+        promptText.gameObject.SetActive(false);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Door1"))
         {
-            UnityEngine.Debug.Log("DOOR1");
+            promptText.text = FirstDoor;
+            promptText.gameObject.SetActive(true);
         }
 
         if (other.CompareTag("Door2"))
         {
-            UnityEngine.Debug.Log("DOOR2");
+            promptText.text = SecondDoor;
+            promptText.gameObject.SetActive(true);
         }
 
         if (other.CompareTag("Door3"))
         {
-            UnityEngine.Debug.Log("DOOR3");
+            promptText.text = ThirdDoor;
+            promptText.gameObject.SetActive(true);
         }
     }
 }
